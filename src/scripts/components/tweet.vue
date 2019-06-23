0e4106d01f0d5b97734c46806fc19625cbd74cb8
DKuwalek <d.kuwalek@gmail.com>
2019-06-23 12:07:00 +0200

    Coco Tweets

--- src/scripts/components/tweet.vue
+++ src/scripts/components/tweet.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="tweet">
+        <img src="https://m.media-amazon.com/images/S/aplus-media/vc/e0636a6f-30e8-4e58-9005-6a234e4feaa4._CR0,0,300,400_PT0_SX300__.png" alt="El Coco">
         Kokos zielony i słodki
         </div>
 </template>
@@ -12,5 +13,10 @@ export default {
 </script>
 
 <style>
-
+.tweet {
+    color: green
+}
+img {
+    width: 50px
+}
 </style>
