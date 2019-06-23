2224b08af13f85faa929cfffccd7599cc15f3c14
DKuwalek <d.kuwalek@gmail.com>
2019-06-23 11:57:46 +0200

    App and Tweet components
    Main component for App
    Component for messages (Tweet

---
+++ src/scripts/components/tweet.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="tweet">
+        Kokos zielony i słodki
+        </div>
+</template>
+
+// Ecma script modules Export default oznacza ze epxportujemy
+<script>
+export default {
+    name: "tweet"
+}
+</script>
+
+<style>
+
+</style>
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('Hello world');
+import App from '@/components/app';
+import Vue from 'vue';
+
+// eslint-disable-next-line no-new
+new Vue({
+    el: '#app',
+    components: {
+        App
+    }
+});
